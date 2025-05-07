9c05531ceba9f50ae4d15f8859b880d46d94be36
Luis Villavicencio <villavicenciovegaluis@gmail.com>
2025-05-07 01:16:05 -0400

    Backup estable: app funcionando en Expo SDK 52 y Firebase ok

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,11 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import {
   initializeAuth,
   getReactNativePersistence
 } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getFirestore } from 'firebase/firestore'; // 👈 agrega Firestore
 
-// 🔐 Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBVVk3jY11GyhmMMdzCY8C-aPr_YgxsuDU",
   authDomain: "lavacontrol-3a7b5.firebaseapp.com",
@@ -16,16 +15,12 @@ const firebaseConfig = {
   appId: "1:545106241602:web:f4292db921dab65e71a5db"
 };
 
-// 🚀 Inicializa la app
 const app = initializeApp(firebaseConfig);
 
-// 🔒 Inicializa Auth con persistencia de sesión
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
 
-// 🔥 Inicializa Firestore
 const db = getFirestore(app);
 
-// 📦 Exporta todo
 export { app, auth, db };
